58985942f92485c352f94a00be47d0c512953f47
okHadi <khan.hadi2951@gmail.com>
2022-12-18 15:38:08 +0500

    Work with prediction data

--- app.c
+++ app.c
@@ -3,6 +3,7 @@
 #include <curl/curl.h>
 #include <string.h>
 #include <ctype.h>
+#define MAX_LINE_LENGTH 1000
 #define SIZE 300
 
 struct memory{                  //struct to store the data from curl
@@ -48,7 +49,8 @@ int main(){
     chnk.size = 0;              //initializes the memory
 
     curl = curl_easy_init();
-    if(curl) {
+    if(curl)
+    {
         struct curl_slist *chunk = NULL;
         chunk = curl_slist_append(chunk, "X-CMC_PRO_API_KEY: d49283ec-042d-49c9-8a38-945fb4d99f98");
         curl_easy_setopt(curl, CURLOPT_HTTPHEADER, chunk);
@@ -58,35 +60,69 @@ int main(){
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, &chnk);           //writes the data to chnk by a pointer
         response = curl_easy_perform(curl);
 
-        if(response != CURLE_OK) {
+        if(response != CURLE_OK) 
+        {
             fprintf(stderr, "Request failed: %s\n", curl_easy_strerror(response));
         }
-        else{
+        else
+        {
+
+        /***************************************PRICE EXTRACTION*******************************************************/
             char *priceStr = NULL;
             priceStr = strstr(chnk.memory, "price");
             int index = priceStr-chnk.memory;
             char finalPrice[20];
-            for (int i=7,j=0; i<16; i++, j++){
-                if(isdigit(priceStr[i]) || ispunct(priceStr[i])){
+            for (int i=7,j=0; i<16; i++, j++)
+            {
+                if(isdigit(priceStr[i]) || ispunct(priceStr[i]))
+                {
                 finalPrice[j] = priceStr[i];
                 }
             }
             puts(finalPrice);
         }
-        curl_easy_cleanup(curl);
-        
+        curl_easy_cleanup(curl);    
     }
     curl_global_cleanup();
 
+    /**************************************PREDICTION EXTRACTION****************************************************/
+    FILE    *textfile;
+    char    line[MAX_LINE_LENGTH];
+    char *p;
+    char* avg23;
+    char* avg24;
+    char* avg25;
+    char* price23;
+    char* price24;
+    char* price25;
+    float pavg23;
+    float pavg24;
+    float pavg25;
+    textfile = fopen("predictions.txt", "r");
+    if(textfile == NULL)
+        puts("\n Could not find the predictions data.");
+    
+    while(fgets(line, MAX_LINE_LENGTH, textfile))
+    {
+        if(strstr(line, symbol))
+        {
+            int j=0;
+            p = strchr(line, '{');
+            for(int i = 0; i<strlen(line), i++;)
+            {
+                if( (isdigit(line[i]) || ispunct(line[i])) && line[i] != ','){
+                    price23[j] = line[i];
+                    j++;
+                }
+            }
+            puts(price23);
+        }
+    }
+    fclose(textfile);
+
+
+
+
     return 0;
-}
-
-
-
-
-
-
-char* getCryptoData(char symbol[], char data[]){
-
 }
 
